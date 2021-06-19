0b605c964cc7186d7d59fad29f182b314ae66f56
tobiprag <prag.tobi@gmail.com>
2021-06-19 15:20:28 +0200

    update 19/6

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
-function myFunction() {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-//   document.getElementById("myBar").style.width = scrolled + "%";
-    var marginStart = -10;
-    var bgStart = 55;
-    // var bg = "-webkit-linear-gradient(#eee " + (bgStart + (scrolled * 0.2)) + "%, #266288 55%)";
-    // console.log(bg);
-    // console.log(scrolled)
-    document.getElementById("main").style.marginTop = marginStart + (scrolled * 0.2) + "rem";
-    document.getElementById("header__name").style.background = "-webkit-linear-gradient(#eee " + ((bgStart + (scrolled * 0.2)) * 1.2) + "%, #266288 55%)";
+// function myFunction() {
+//     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+//     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//     var scrolled = (winScroll / height) * 100;
 
 
 
-if (document.documentElement.scrollTop == 0) {
-    document.getElementById("main").style.marginTop = marginStart + "rem";
-} else if (document.getElementById("main").style.marginTop < "-5rem") {
-    document.getElementById("main").style.marginTop = -5 + "rem"
+
+// //   document.getElementById("myBar").style.width = scrolled + "%";
+
+
+
+
+
+//     var marginStart = -10;
+//     var bgStart = 55;
+
+
+
+
+//     // var bg = "-webkit-linear-gradient(#eee " + (bgStart + (scrolled * 0.2)) + "%, #266288 55%)";
+//     // console.log(bg);
+//     // console.log(scrolled)
+
+
+
+
+
+//     document.getElementById("main").style.marginTop = marginStart + (scrolled * 0.2) + "rem";
+//     document.getElementById("header__name").style.background = "-webkit-linear-gradient(#eee " + ((bgStart + (scrolled * 0.2)) * 1.2) + "%, #266288 55%)";
+
+
+
+// if (document.documentElement.scrollTop == 0) {
+//     document.getElementById("main").style.marginTop = marginStart + "rem";
+// } else if (document.getElementById("main").style.marginTop < "-5rem") {
+//     document.getElementById("main").style.marginTop = -5 + "rem"
+// }
+
+
+
+// } 
+var menu = document.getElementById("menu");
+
+
+function openMenu() {
+    
+    menu.style.background = "blue"
 }
-
-
-
-} 
